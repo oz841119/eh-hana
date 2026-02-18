619ee7662748a39515985681918c77c0e5da5a20
Yili <oz841119@gmail.com>
2026-02-18 23:53:18 +0800

    e

--- packages/database/index.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client";
-
-const connectionString = `${process.env.DATABASE_URL}`;
-const createPrismaPg = () => {
-  return new PrismaPg({ connectionString });
-};
-export { createPrismaPg, PrismaClient };
