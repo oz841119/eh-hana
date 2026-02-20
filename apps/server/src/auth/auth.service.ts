f7ceb4b72db8aa9d4d5bb74049c8816859b5a67e
Yili <oz841119@gmail.com>
2026-02-20 23:45:17 +0800

    w

---
+++ apps/server/src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  constructor() {}
+  // async validateOAuthUser(profile: any) {}
+  // async login(user: User) {
+  //   return {
+  //     access_token: '',
+  //   };
+  // }
+}
